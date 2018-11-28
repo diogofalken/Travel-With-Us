8a9b81a748bdde6f2e527112112107d9d098ce22
diogofalken <diogofalken0@gmail.com>
2018-11-28 18:23:04 +0000

    Added js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,23 @@
-function sideMenu() {
-  if (document.getElementById("mySidenav").style.width === "250px") {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
-  } else {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+/* SlideShow */
+var i = 1;
+var slide = document.getElementById("slideshow");
+var slide_text = document.getElementById("text");
+var images = [
+  "../images/tokyo.jpeg",
+  "../images/madrid.jpeg",
+  "../images/newyork.jpeg",
+  "../images/lisboa.jpeg"
+];
+var text = ["Tokyo", "Madrid", "New York", "Lisboa"];
+
+function slideshow() {
+  if (i == 3) {
+    i = 0;
   }
+  slide.style.backgroundImage = "url(" + images[i] + ")";
+  slide_text.innerHTML = text[i];
+  i++;
+  setTimeout("slideshow()", 3000);
 }
+
+window.onload = slideshow;
