149fd1460c2155d7bed7b522cf3bc069fc1055d4
diogofalken <diogofalken0@gmail.com>
2018-12-08 22:01:08 +0000

    Working on main page

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-/* SlideShow */
+/* ---------------- SlideShow ---------------- */
 var i = 1;
 var slide = document.getElementById("slideshow");
 var slide_text = document.getElementById("text");
@@ -6,16 +6,18 @@ var images = [
   "../images/tokyo.jpeg",
   "../images/madrid.jpeg",
   "../images/newyork.jpeg",
-  "../images/lisboa.jpeg"
+  "../images/lisboa.jpeg",
+  "../images/london.jpeg"
 ];
-var text = ["Tokyo", "Madrid", "New York", "Lisboa"];
+var text = ["Tokyo", "Madrid", "New York", "Lisboa", "London"];
 
 function slideshow() {
-  if (i == 3) {
+  if (i == 5) {
     i = 0;
   }
   slide.style.backgroundImage = "url(" + images[i] + ")";
   slide_text.innerHTML = text[i];
+  slide.style.transition = "opacity 1s ease-out";
   i++;
   setTimeout("slideshow()", 3000);
 }
