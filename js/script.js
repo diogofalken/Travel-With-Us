4cd9a6bbc95766ce2b863fe7b608d16e066dd67d
diogofalken <diogofalken0@gmail.com>
2018-12-09 22:04:41 +0000

    Finished Footer, added time

--- js/script.js
+++ js/script.js
@@ -22,4 +22,33 @@ function slideshow() {
   setTimeout("slideshow()", 3000);
 }
 
+/* Footer Date */
+function time() {
+  var date = new Date();
+  var horas = date.getHours();
+  var minutos = date.getMinutes();
+  var segundos = date.getSeconds();
+  minutos = checkTime(minutos);
+  segundos = checkTime(segundos);
+
+  document.getElementById("hora").innerHTML =
+    horas + ":" + minutos + ":" + segundos;
+  var t = setTimeout(time, 500);
+}
+
+function checkTime(i) {
+  if (i < 10) {
+    i = "0" + i;
+  } // adiciona zero na esquerda dos numeros < 10
+  return i;
+}
+
+// Visto que nao existe maneira de ter duas funcoes a dar window.load ao mesmo tempo tive de fazer esta funcao
 window.onload = slideshow;
+var current = window.onload;
+window.onload = function() {
+  if (current) {
+    current();
+  }
+  time();
+};
