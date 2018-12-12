6a65375ed86236fb421041f847444a2612c4e14b
diogofalken <diogofalken0@gmail.com>
2018-12-12 10:59:17 +0000

    Divided all my content in more css/js files to be easy to use them all

---
+++ js/index.js
@@ -0,0 +1,24 @@
+/* ---------------- SlideShow ---------------- */
+var i = 1;
+var slide = document.getElementById("slideshow");
+var slide_text = document.getElementById("text");
+var images = [
+  "../images/tokyo.jpeg",
+  "../images/madrid.jpeg",
+  "../images/newyork.jpeg",
+  "../images/lisboa.jpeg",
+  "../images/london.jpeg"
+];
+var text = ["Tokyo", "Madrid", "New York", "Lisbon", "London"];
+
+function slideshow() {
+  if (i == 5) {
+    i = 0;
+  }
+  slide.style.backgroundImage = "url(" + images[i] + ")";
+  slide_text.innerHTML = text[i];
+  i++;
+
+  setTimeout("slideshow()", 3000);
+}
+slideshow();
--- js/script.js
+++ js/script.js
@@ -1,28 +1,5 @@
 window.onscroll = toogleScrollButton;
-/* ---------------- SlideShow ---------------- */
-var i = 1;
-var slide = document.getElementById("slideshow");
-var slide_text = document.getElementById("text");
-var images = [
-  "../images/tokyo.jpeg",
-  "../images/madrid.jpeg",
-  "../images/newyork.jpeg",
-  "../images/lisboa.jpeg",
-  "../images/london.jpeg"
-];
-var text = ["Tokyo", "Madrid", "New York", "Lisboa", "London"];
 
-function slideshow() {
-  if (i == 5) {
-    i = 0;
-  }
-  slide.style.backgroundImage = "url(" + images[i] + ")";
-  slide_text.innerHTML = text[i];
-  i++;
-
-  setTimeout("slideshow()", 3000);
-}
-slideshow();
 /* Footer Date */
 function time() {
   var date = new Date();
