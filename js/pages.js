f048864d2e7884c5ac9313f5b6279e66389782d3
diogofalken <diogofalken0@gmail.com>
2018-12-15 09:02:04 +0000

    Still working on Tokyo Page

---
+++ js/pages.js
@@ -0,0 +1,12 @@
+var read_more = document.getElementById("read-more");
+var more_info = document.getElementById("more");
+
+read_more.addEventListener("click", function() {
+  if (more_info.style.display == "block") {
+    more_info.style.display = "none";
+    read_more.innerHTML = "More";
+  } else {
+    more_info.style.display = "block";
+    read_more.innerHTML = "Close";
+  }
+});
